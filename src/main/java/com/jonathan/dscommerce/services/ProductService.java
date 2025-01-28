bab027bd589d3f3eeb79fad17cb2810fea31eea6
jonathan.silva <jonathan.msdev@gmail.com>
2025-01-27 21:19:01 -0300

    ProductMinDTO

--- src/main/java/com/jonathan/dscommerce/services/ProductService.java
+++ src/main/java/com/jonathan/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.jonathan.dscommerce.services;
 
 import com.jonathan.dscommerce.dto.ProductDTO;
+import com.jonathan.dscommerce.dto.ProductMinDTO;
 import com.jonathan.dscommerce.entities.Product;
 import com.jonathan.dscommerce.repositories.ProductRepository;
 import com.jonathan.dscommerce.services.exceptions.DatabaseException;
@@ -28,9 +29,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
