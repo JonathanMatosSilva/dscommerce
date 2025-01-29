cdc1d007357cf2e068fdd0af0638cdb0debcfdae
jonathan.silva <jonathan.msdev@gmail.com>
2025-01-28 21:17:38 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/jonathan/dscommerce/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.jonathan.dscommerce.services;
+
+import com.jonathan.dscommerce.dto.OrderDTO;
+import com.jonathan.dscommerce.entities.Order;
+import com.jonathan.dscommerce.repositories.OrderRepository;
+import com.jonathan.dscommerce.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Transactional(readOnly = true)
+    public OrderDTO findById(Long id) {
+        Order order = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new OrderDTO(order);
+    }
+}
